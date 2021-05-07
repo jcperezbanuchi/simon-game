f58f404263125c005f30e11be5e5f6a0792c5d2d
Juan Perez Banuchi <juanco@Juans-Air.attlocal.net>
2021-05-07 16:41:50 -0400

    add reset button function

--- app.js
+++ app.js
@@ -1,21 +1,44 @@
-// let start = false;
-// let player = []
-// let count = []
+let start = false;
+let win = false
+let player = []
+let randomOrder = []
+let computer;
 
 
-// const startBtn = document.querySelector('#start')
-// const topLeft = document.querySelector('#top-left')
-// const topRight = document.querySelector('#top-left')
-// const bottomLeft = document.querySelector('#top-left')
-// const bottomRight = document.querySelector('#top-left')
-// const reset = document.querySelector('#reset');
+const startBtn = document.querySelector('#start')
+const green = document.querySelector('#top-left')
+const red = document.querySelector('#top-right')
+const yellow = document.querySelector('#bottom-left')
+const blue = document.querySelector('#bottom-right')
+const reset = document.querySelector('#reset');
+const round = document.querySelector('#round')
 
 
 
 
-// const startGame = () => {
+const startGame = () => {
+    alert(`You're about to start the game! `);
+    win = false;
+    randomOrder = [];
+    player = [];
 
-// }
+    round.innerText = '1';
+}
 
 
-// startBtn.addEventListener('click', startGame)
+
+const resetGame = () => {
+    start = false;
+    win = false
+    player = []
+    randomOrder = []
+    computer = false
+    round.innerText = '0'
+}
+
+startBtn.addEventListener('click', startGame)
+reset.addEventListener('click', resetGame)
+
+
+
+
