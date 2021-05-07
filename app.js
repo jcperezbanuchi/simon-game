9afaf8c0a99d0c47258a08b043b3efde9429dcbe
Juan Perez Banuchi <juanco@Juans-Air.attlocal.net>
2021-05-07 15:57:26 -0400

    using DOM grabbed all buttons

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+// let start = false;
+// let player = []
+// let count = []
 
+
+// const startBtn = document.querySelector('#start')
+// const topLeft = document.querySelector('#top-left')
+// const topRight = document.querySelector('#top-left')
+// const bottomLeft = document.querySelector('#top-left')
+// const bottomRight = document.querySelector('#top-left')
+// const reset = document.querySelector('#reset');
+
+
+
+
+// const startGame = () => {
+
+// }
+
+
+// startBtn.addEventListener('click', startGame)
