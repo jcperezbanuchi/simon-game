628aa48fe06a6688e175517c5e5324eb7ec92d95
Juan Perez Banuchi <juanco@Juans-Air.attlocal.net>
2021-05-10 13:02:35 -0400

    add more to the event listeners

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 let win = false
 let player = []
 let sequence = []
-let rounds = 0
-let colors = ["red", "blue", "green", "yellow"]
+let rounds = 0;
+let flashes;
+let computerTurn = false;
 
 
+
+const allCircle = document.querySelectorAll('.circle')
 const startBtn = document.querySelector('#start')
 const green = document.querySelector('#top-left')
 const red = document.querySelector('#top-right')
@@ -22,22 +25,47 @@ const startGame = () => {
     sequence = [];
     player = [];
     round.innerText = '1';
-
+    originalColor();
+    for (let i = 0; i < 20; i++) {
+        randomNumber = (Math.floor(Math.random() * 4) + 1)
+        sequence.push(randomNumber)
+    }
+    computerTurn = true
+    nextRound()
 }
 
+// this idea was taken from https://github.com/beaucarnes/simon-game/blob/master/js/index.js
 const nextRound = () => {
-    rounds++
-    randomOrder = Math.floor(Math.random() * 4)
-    sequence.push(colors[randomOrder])
-    player = []
-    circle.classList.add('disable');
+    rounds++;
+    // allCircle.classList.add('disable');
     originalColor()
+
+    if (computerTurn === true) {
+        originalColor()
+        setTimeout(() => {
+            if (sequence[flashes] === 1) {
+                green.style.backgroundColor = "lightgreen";
+            }
+            if (sequence[flashes] === 2) {
+                red.style.backgroundColor = "red"
+            }
+            if (sequence[flashes] === 3) {
+                yellow.style.backgroundColor = "yellow"
+            }
+            if (sequence[flashes] === 4) {
+                blue.style.backgroundColor = "blue"
+            }
+            flashes++;
+        }, 200);
+    }
+
+    checkWinner()
 }
 
 const resetGame = () => {
     win = false
     player = []
-    randomOrder = []
+    sequence = []
     computer = false
     round.innerText = '-'
 }
@@ -46,14 +74,15 @@ const resetGame = () => {
 const checkWinner = (currentLevel) => {
     if (sequence[currentLevel] === player[currentLevel]) {
         if (sequence.length === player.length) {
-            setTimeout(function () {
+            setTimeout(() => {
                 nextRound();
             }, 1000);
         } else if (player.length === 20) {
             win = true
             alert('You won!')
             alert('Hit the reset button or refresh the page')
-            resetGame()
+            lightColors();
+            resetGame();
         }
     } else {
         const h2 = document.createElement('h2');
@@ -70,45 +99,54 @@ const originalColor = () => {
     blue.style.backgroundColor = "darkblue";
 }
 
-const
+const lightColors = () => {
+    green.style.backgroundColor = "lightgreen";
+    red.style.backgroundColor = "red";
+    yellow.style.backgroundColor = "yellow";
+    blue.style.backgroundColor = "blue";
+}
 
 
-    startBtn.addEventListener('click', startGame)
+startBtn.addEventListener('click', startGame)
 reset.addEventListener('click', resetGame)
 
 
 green.addEventListener('click', (event) => {
     green.style.backgroundColor = "lightgreen";
+    player.push(1)
     if (win === false) {
         setTimeout(() => {
             originalColor();
-        }, 300);
+        }, 200);
     }
 })
 
 red.addEventListener('click', (event) => {
     red.style.backgroundColor = "red";
+    player.push(2)
     if (win === false) {
         setTimeout(() => {
             originalColor();
-        }, 300);
+        }, 200);
     }
 })
 
 yellow.addEventListener('click', (event) => {
-    yellow.style.backgroundColor = "yellow";
+    yellow.style.backgroundColor = "yellow";]
+    player.push(3)
     if (win === false) {
         setTimeout(() => {
             originalColor();
-        }, 300);
+        }, 200);
     }
 })
 
 blue.addEventListener('click', (event) => {
     blue.style.backgroundColor = "blue";
+    player.push(4)
     if (win === false) {
         setTimeout(() => {
             originalColor();
-        }, 300);
+        }, 200);
     }
 })
