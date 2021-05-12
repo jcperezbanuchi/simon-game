78a45b02af5e195ac0870c145cd36763c772a16a
Juan Perez Banuchi <juanco@Juans-Air.attlocal.net>
2021-05-12 18:44:38 -0400

    fixed more bugs

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ let soundEfects = [
 
 
 //this was inspired by : https://codepen.io/zentech/pen/XaYygR?editors=0010
+//this function works after the start button is clicked and checks what random number is been added to simons array.
 const simonSequence = () => {
     startBtn.setAttribute('disabled', 'disabled');
     rounds++
@@ -52,16 +53,16 @@ const simonSequence = () => {
         if (simon.length === i) {
             clearTimeout(interval)
         }
-    }, 1000)
+    }, 800)
 
 }
-
+//generates a random number each time the function is called and add it to simons array
 const randomNumber = () => {
     let random = Math.floor(Math.random() * 4)
     simon.push(random)
 }
 
-
+// this function give each color a lighter one after they're called or clicked
 const activateColor = (id, color) => {
     document.getElementById(id).classList.add(color + '-active')
     playSound(id)
@@ -70,22 +71,21 @@ const activateColor = (id, color) => {
     }, 500);
 }
 
+// this function checks the player sequence after every click
 const playerSequence = () => {
     if (checkSequence() === false) {
         wrongColor()
         simonSequence()
-
     } else if (player.length === simon.length && player.length < totalRounds) {
         simonSequence()
         player = []
-
     } if (player.length === totalRounds) {
         alert('You win the game!')
         resetGame()
     }
 
 }
-
+// this function check if simon and players array are the same
 const checkSequence = () => {
     for (let i = 0; i < player.length; i++) {
         if (player[i] !== simon[i]) {
@@ -108,7 +108,7 @@ const resetGame = () => {
 //click the wrong color 
 const wrongColor = () => {
     let wrong = setInterval(() => {
-        alert('You click the wrong color! start again')
+        alert('You click the wrong color! Start again')
         resetGame()
         clearInterval(wrong)
     }, 150)
@@ -122,83 +122,69 @@ const playSound = (id) => {
 }
 
 
-//event listeners
-
-
-
-green.addEventListener('click', () => {
-    id = green.getAttribute('id')
-    color = green.getAttribute('class').split(' ')[1]
-    green.classList.add(color + '-active')
-    setTimeout(() => {
-        green.classList.remove(color + '-active')
-    }, 300);
-    player.push(parseInt(id))
-    activateColor(id, color)
-    playerSequence()
-})
-
-red.addEventListener('click', () => {
-    id = red.getAttribute('id')
-    color = red.getAttribute('class').split(' ')[1]
-    red.classList.add(color + '-active')
-    setTimeout(() => {
-        red.classList.remove(color + '-active')
-    }, 300);
-    player.push(parseInt(id))
-    activateColor(id, color)
-    playerSequence()
-})
-
-yellow.addEventListener('click', () => {
-    id = yellow.getAttribute('id')
-    color = yellow.getAttribute('class').split(' ')[1]
-    yellow.classList.add(color + '-active')
-    setTimeout(() => {
-        yellow.classList.remove(color + '-active')
-    }, 300);
-    player.push(parseInt(id))
-    activateColor(id, color)
-    playerSequence()
-})
-
-blue.addEventListener('click', () => {
-    id = blue.getAttribute('id')
-    color = blue.getAttribute('class').split(' ')[1]
-    blue.classList.add(color + '-active')
-    setTimeout(() => {
-        blue.classList.remove(color + '-active')
-    }, 300);
-    player.push(parseInt(id))
-    activateColor(id, color)
-    playerSequence()
-})
-
-/////////// modal ///////////////////
+////////event listeners////
 window.addEventListener('DOMContentLoaded', () => {
-    // Grabbing About the Game button
+    //modal
     const openBtn = document.querySelector('#openModal');
-
-    // Grabbing modal element
     const modal = document.querySelector('#modal');
-
-    // Grabbing close button
     const closeBtn = document.querySelector('#close');
-
     openBtn.addEventListener('click', openModal);
-
     closeBtn.addEventListener('click', closeModal);
 
+    // start and reset
+    startBtn.addEventListener('click', simonSequence)
     reset.addEventListener('click', resetGame)
 
-
-    startBtn.addEventListener('click', simonSequence)
+    /////green
+    green.addEventListener('click', () => {
+        id = green.getAttribute('id')
+        color = green.getAttribute('class').split(' ')[1]
+        green.classList.add(color + '-active')
+        setTimeout(() => {
+            green.classList.remove(color + '-active')
+        }, 300);
+        player.push(parseInt(id))
+        activateColor(id, color)
+        playerSequence()
+    })
+    ///red
+    red.addEventListener('click', () => {
+        id = red.getAttribute('id')
+        color = red.getAttribute('class').split(' ')[1]
+        red.classList.add(color + '-active')
+        setTimeout(() => {
+            red.classList.remove(color + '-active')
+        }, 300);
+        player.push(parseInt(id))
+        activateColor(id, color)
+        playerSequence()
+    })
+    ////yellow
+    yellow.addEventListener('click', () => {
+        id = yellow.getAttribute('id')
+        color = yellow.getAttribute('class').split(' ')[1]
+        yellow.classList.add(color + '-active')
+        setTimeout(() => {
+            yellow.classList.remove(color + '-active')
+        }, 300);
+        player.push(parseInt(id))
+        activateColor(id, color)
+        playerSequence()
+    })
+    /////blue
+    blue.addEventListener('click', () => {
+        id = blue.getAttribute('id')
+        color = blue.getAttribute('class').split(' ')[1]
+        blue.classList.add(color + '-active')
+        setTimeout(() => {
+            blue.classList.remove(color + '-active')
+        }, 300);
+        player.push(parseInt(id))
+        activateColor(id, color)
+        playerSequence()
+    })
 
 });
-const disableBtn = () => {
-    startBtn.disable = 'true'
-}
-
 
 const openModal = () => {
     modal.style.display = 'block'
@@ -207,5 +193,3 @@ const openModal = () => {
 const closeModal = () => {
     modal.style.display = 'none'
 }
-
-
